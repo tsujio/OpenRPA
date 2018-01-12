038f846abba7b7a9cbbdbac9df7d512e67a17c88
tsujio <ntsujio@gmail.com>
2018-01-13 04:04:54 +0900

    Add downloading robot code

--- src/OpenRPA.Interpreter/RobotInterpreter.cs
+++ src/OpenRPA.Interpreter/RobotInterpreter.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +14,38 @@ namespace OpenRPA.Interpreter
 
         private string robotId;
 
+        private string RobotDownloadUrl
+        {
+            get
+            {
+                return this.serverUrl + "/workflow/" + this.robotId;
+            }
+        }
+
+        private string TempAppDir
+        {
+            get
+            {
+                return Path.Combine(Path.GetTempPath(), "OpenRPA");
+            }
+        }
+
+        private string RobotCacheDir
+        {
+            get
+            {
+                return Path.Combine(TempAppDir, "cache");
+            }
+        }
+
+        private string RobotFileName
+        {
+            get
+            {
+                return robotId + ".rpa";
+            }
+        }
+
         public RobotInterpreter(string serverUrl, string robotId)
         {
             this.serverUrl = serverUrl;
@@ -20,7 +54,38 @@ namespace OpenRPA.Interpreter
 
         public void Execute()
         {
+            if (!Directory.Exists(TempAppDir))
+            {
+                Directory.CreateDirectory(TempAppDir);
+            }
+            if (!Directory.Exists(RobotCacheDir))
+            {
+                Directory.CreateDirectory(RobotCacheDir);
+            }
 
+            string path = DownloadRobot(RobotDownloadUrl);
+        }
+
+        private string DownloadRobot(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync(url).Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception("Failed to download robot");
+                }
+
+                var stream = response.Content.ReadAsStreamAsync().Result;
+
+                var path = Path.Combine(TempAppDir, RobotFileName);
+                using (var f = File.Create(path))
+                {
+                    stream.CopyTo(f);
+                }
+
+                return path;
+            }
         }
     }
 }
